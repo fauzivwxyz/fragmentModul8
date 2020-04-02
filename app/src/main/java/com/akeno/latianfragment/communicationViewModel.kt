538ab4889fd960ae58ac1d68e3cf61d58009728b
fauzi <fauziofficial10@gmail.com>
2020-04-02 16:49:47 +0700

    versi pertama

---
+++ app/src/main/java/com/akeno/latianfragment/communicationViewModel.kt
@@ -0,0 +1,15 @@
+package com.akeno.latianfragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class communicationViewModel : ViewModel() {
+    private val mName = MutableLiveData<String>()
+
+    val name: LiveData<String>
+        get() = mName
+
+    fun setName(name: String) {
+        mName.value = name
+    }
+}
